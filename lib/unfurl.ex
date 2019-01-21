ce8c1877fdd00296c19c5fdcc86660a457c131e7
Eric Teubert <eric@ericteubert.de>
2019-01-21 13:05:09 +0100

    fetch title from URL

--- lib/unfurl.ex
+++ lib/unfurl.ex
@@ -3,16 +3,16 @@ defmodule Unfurl do
   Documentation for Unfurl.
   """
 
-  @doc """
-  Hello world.
+  def unfurl(url) do
+    {:ok, response} = HTTPoison.get(url)
+    %HTTPoison.Response{status_code: 200, headers: _headers, body: html} = response
 
-  ## Examples
+    title =
+      Floki.find(html, "meta[property=\"og:title\"")
+      |> Floki.attribute("content")
+      |> hd
 
-      iex> Unfurl.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    result = %{title: title}
+    {:ok, result}
   end
 end
--- test/unfurl_test.exs
+++ test/unfurl_test.exs
@@ -1,8 +1,12 @@
 defmodule UnfurlTest do
   use ExUnit.Case
-  doctest Unfurl
+  # doctest Unfurl
 
-  test "greets the world" do
-    assert Unfurl.hello() == :world
+  import Unfurl, only: [unfurl: 1]
+
+  test "extracts page title" do
+    {:ok, result} = Unfurl.unfurl("https://freakshow.fm/fs229-telefonischturm")
+
+    assert result.title == "FS229 Telefonischturm"
   end
 end
