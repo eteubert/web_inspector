49f46e73423ea300b786238509ac2e802d61377e
Eric Teubert <eric@ericteubert.de>
2019-01-21 18:34:35 +0100

    perf: parallelize parsing

--- lib/unfurl.ex
+++ lib/unfurl.ex
@@ -9,15 +9,21 @@ defmodule Unfurl do
     {:ok, response} = HTTPoison.get(url)
     %HTTPoison.Response{status_code: 200, headers: _headers, body: html} = response
 
-    open_graph = OpenGraph.parse(html)
-    twitter = Twitter.parse(html)
+    tasks = [
+      Task.async(OpenGraph, :parse, [html]),
+      Task.async(Twitter, :parse, [html])
+    ]
 
-    result =
-      %{providers: %{}}
-      |> put_in([:providers, :open_graph], open_graph)
-      |> put_in([:providers, :twitter], twitter)
-      |> Map.put(:title, Map.get(open_graph, "title"))
+    with [open_graph, twitter] <- Task.yield_many(tasks),
+         {_, {:ok, open_graph}} <- open_graph,
+         {_, {:ok, twitter}} <- twitter do
+      result =
+        %{providers: %{}}
+        |> put_in([:providers, :open_graph], open_graph)
+        |> put_in([:providers, :twitter], twitter)
+        |> Map.put(:title, Map.get(open_graph, "title"))
 
-    {:ok, result}
+      {:ok, result}
+    end
   end
 end
