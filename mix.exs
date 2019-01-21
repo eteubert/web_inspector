90613c6975607e4ea8e1dd24a2227e52316f5dbe
Eric Teubert <eric@ericteubert.de>
2019-01-21 13:04:49 +0100

    add hex packages: httpoison, jason, floki

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule Unfurl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.5"},
+      {:jason, "~> 1.1"},
+      {:floki, "~> 0.20.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
