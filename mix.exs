b7755a58fa1d5a84d0b7fc3067a0383edf108bc9
Eric Teubert <eric@ericteubert.de>
2020-11-18 21:29:40 +0100

    chore(deps): update bypass

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule WebInspector.MixProject do
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.2.2"},
       {:floki, "~> 0.29.0"},
-      {:bypass, "~> 1.0", only: :test}
+      {:bypass, "~> 2.1", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
