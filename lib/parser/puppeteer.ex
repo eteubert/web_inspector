969dce0d158c79ef793604652fbfcb55c849d668
Eric Teubert <eric@ericteubert.de>
2021-03-18 21:38:00 +0100

    feat: add Puppeteer function to fetch screenshot

--- lib/parser/puppeteer.ex
+++ lib/parser/puppeteer.ex
@@ -9,6 +9,27 @@ defmodule WebInspector.Parser.Puppeteer do
     end
   end
 
+  def fetch_screenshot(url) do
+    headers = []
+
+    options = [recv_timeout: 30_000]
+
+    host = Application.get_env(:web_inspector, :puppeteer_host)
+    request_url = host <> "/screenshot/?" <> URI.encode_query(%{"url" => url})
+
+    case HTTPoison.get(
+           request_url,
+           headers,
+           options
+         ) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: jpeg}} ->
+        jpeg
+
+      _ ->
+        ""
+    end
+  end
+
   defp do_fetch_and_parse(url) do
     headers = []
 
