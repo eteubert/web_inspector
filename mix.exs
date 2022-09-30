7f7857d32b5e12c9ccef871f900acd9ebe8e8637
Eric Teubert <eric@ericteubert.de>
2022-09-30 11:50:03 +0200

    chore!: increase elixir version requirement
    BREAKING: I want to use URI.append_query/2

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule WebInspector.MixProject do
   def project do
     [
       app: :web_inspector,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: "1.0.0",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
